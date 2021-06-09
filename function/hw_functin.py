b6a24962c8dbc74fbaa220096a46a19db3e7ee81
mamuka96 <mamuka2281488@gmail.com>
2021-06-09 15:00:07 +0300

    ha've donу copy

--- function/hw_functin.py
+++ function/hw_functin.py
@@ -2,7 +2,7 @@
 
 from random import randint
 
-n = (15)
+n = 15
 list_of_num = []
 for i in range(n):
     list_of_num.append(randint(0, 50))
@@ -15,35 +15,37 @@ def bobbles(sort_list):
         for x in range(0, last_item):
             if sort_list[x] > sort_list[x+1]:
                 sort_list[x], sort_list[x + 1] = sort_list[x + 1], sort_list[x]
-    return (sort_list)
 
-print(list_of_num)
+    return sort_list
 
-new_list = bobbles(list_of_num).copy()
-print(new_list)
+
+sort_list = list_of_num.copy()
+print(sort_list)
+print(bobbles(list_of_num))
+
 
 
 # Task2
 
-def user_enter():
-    for i in range(7):
-        user_input = input('Enter your favorite number: ')
-        print(i)
-        try:
-            number = int(user_input)
-            print('success')
-        except ValueError as err:
-            if i < 3:
-                print("wrong number")
-            elif i < 5:
-                print("wrong number, just a few attempts left")
-            elif i == 5:
-                print('last chance')
-            else:
-                print('fail')
-            continue
-        break
-
-
-user_enter()
+# def user_enter():
+#     for i in range(7):
+#         user_input = input('Enter your favorite number: ')
+#         print(i)
+#         try:
+#             number = int(user_input)
+#             print('success')
+#         except ValueError as err:
+#             if i < 3:
+#                 print("wrong number")
+#             elif i < 5:
+#                 print("wrong number, just a few attempts left")
+#             elif i == 5:
+#                 print('last chance')
+#             else:
+#                 print('fail')
+#             continue
+#         break
+#
+#
+# user_enter()
 
